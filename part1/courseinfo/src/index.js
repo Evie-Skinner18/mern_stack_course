cee0e8dff6d8a2257ec70c7dc98213a80e127fce
Evie-Skinner18 <eskinner@spartaglobal.com>
2019-11-02 16:13:53 +0000

    Created a re-useable Part component so the Content component can be chunked down

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -7,19 +7,23 @@ const Header = (props) => {
   )
 }
 
-const Content = (props) => {
+const Part = (props) => {
   return (
     <>
       <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
+        {props.part} {props.exercises}
       </p>
     </>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.part1} exercises={props.exercises1}/>
+      <Part part={props.part2} exercises={props.exercises2}/>
+      <Part part={props.part3} exercises={props.exercises3}/>
+    </div>
   )
 }
 
@@ -42,9 +46,7 @@ const App = () => {
     return (
       <div>
         <Header course ={course}/>
-        <Content part1={part1}/>
-        <Content part2={part2}/>
-        <Content part3={part3}/>
+        <Content part1={part1} part2={part2} part3={part3}/>
         <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
       </div>
     )
