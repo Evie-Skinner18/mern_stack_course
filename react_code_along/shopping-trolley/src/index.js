131c876b62aa9afcde17be4714930b5261e9b082
Evie-Skinner18 <eskinner@spartaglobal.com>
2019-12-31 14:38:51 +0000

    Re-added index.js and created a JSX h1 element which is rendered in root div of index.html

---
+++ react_code_along/shopping-trolley/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const element = <h1>Hello World!</h1>;
+let rootDiv = document.getElementById('root');
+ReactDOM.render(element, rootDiv);
